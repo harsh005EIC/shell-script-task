2f22390b97db269fdfd418ccd5e69b2dacadbd12
harsh_kerai <harsh.kerai@einfochips.com>
2026-02-16 14:02:10 +0530

    remove all files from release

--- 170877_harsh_shellscript_q4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/harsh005/shell_script/functions/170877_harsh_shellscript_q4_function.sh
-
-while true;do
-read -r -p "Enter a string (or exit) : " str
-[[ $str == "exit" ]] && echo "Exiting...." && break
-check_rev_string "$str" || continue
-done
-
